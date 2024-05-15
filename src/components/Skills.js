4cca331b6dd590432dd784d9413be966ce2a9d1e
srgjo27 <96277339+srgjo27@users.noreply.github.com>
2024-05-15 13:15:37 +0700

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,24 +1,21 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
     const responsive = {
         superLargeDesktop: {
             breakpoint: { max: 4000, min: 3000 },
-            items: 5,
+            items: 8,
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 4,
+            items: 8,
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 2,
+            items: 4,
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
@@ -38,20 +35,69 @@ export const Skills = () => {
                             </p>
                             <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                                 <div className="item">
-                                    <img src={meter1} alt="" />
-                                    <h5>Web Development</h5>
+                                    <img src="
+                                    https://img.icons8.com/color/96/000000/html-5.png" alt="" />
+                                    <h5>HTML</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter3} alt="" />
-                                    <h5>Mobile Development</h5>
+                                    <img src="https://img.icons8.com/color/96/000000/css3.png" alt="" />
+                                    <h5>CSS</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="" />
-                                    <h5>Database Management</h5>
+                                    <img src="https://img.icons8.com/color/96/000000/javascript.png" alt="" />
+                                    <h5>JavaScript</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="" />
-                                    <h5>Mechine Learning</h5>
+                                    <img src="https://img.icons8.com/color/96/000000/nodejs.png" alt="" />
+                                    <h5>Node JS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/tailwindcss.png" alt="" />
+                                    <h5>Tailwind</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/bootstrap.png" alt="" />
+                                    <h5>Bootstrap</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/react-native.png" alt="" />
+                                    <h5>React JS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/php.png" alt="" />
+                                    <h5>PHP</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://cdn.worldvectorlogo.com/logos/laravel-2.svg" alt="" />
+                                    <h5>Laravel</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/python.png" alt="" />
+                                    <h5>Python</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/golang" alt="" />
+                                    <h5>Go</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/java-coffee-cup-logo.png" alt="" />
+                                    <h5>Java</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/dart.png" alt="" />
+                                    <h5>Dart</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/flutter.png" alt="" />
+                                    <h5>Flutter</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/git.png" alt="" />
+                                    <h5>Git</h5>
+                                </div>
+                                <div className="item">
+                                    <img src="https://img.icons8.com/color/96/000000/firebase.png" alt="" />
+                                    <h5>Firebase</h5>
                                 </div>
                             </Carousel>
                         </div>
